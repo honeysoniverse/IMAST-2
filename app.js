5e27ab4b86f18c1f35232a7d654ba7856e04fbf5
honeysoniverse <95574885+honeysoniverse@users.noreply.github.com>
2021-12-23 22:21:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1500})})
+        autoplaySpeed: 2500})})
 
 const btn = document.getElementById('m-menu');
 
